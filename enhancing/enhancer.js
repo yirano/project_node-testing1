106ee051a51895662dffdbdc52a7c51f7fe3f059
Tiffany Arellano <missusfantastic@Missus-Fantastic.local>
2020-08-11 12:30:27 -0400

    Stretch?

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,38 +1,40 @@
 module.exports = {
-	success,
-	fail,
-	repair,
-	get
-}
+  success,
+  fail,
+  repair,
+  get,
+};
 
 function success(item) {
-	if (item.enhancement < 20) {
-		item.enhancement += 1
-	}
-
-	return item
+  if (item.enhancement < 20) {
+    item.enhancement += 1;
+  }
+  return item;
 }
 
 function fail(item) {
-	if (item.enhancement < 15) {
-		item.durability -= 5
-	} else if (item.enhancement >= 15) {
-		if (item.enhancement > 16) {
-			item.enhancement -= 1
-		}
-		item.durability += 10
-	}
-	return item
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      item.enhancement -= 1;
+    }
+    item.durability += 10;
+  }
+  return item;
 }
 
 function repair(item) {
-	if (item.durability <= 100) {
-		const diff = 100 - item.durability
-		item.durability += diff
-	}
-	return item
+  if (item.durability <= 100) {
+    const diff = 100 - item.durability;
+    item.durability += diff;
+  }
+  return item;
 }
 
 function get(item) {
-	return { ...item }
+  const msg = "";
+  if (item.enhancement > 0) {
+    msg = `[${item.enhancement}] ${item.name}`;
+  }
 }
